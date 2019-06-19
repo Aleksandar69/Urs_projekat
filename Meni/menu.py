ba6d32609964439e6537b4e08067fdcef13fb977
Goran Ninkovic <warhawk@hotmail.rs>
2019-06-19 19:41:48 +0200

    print the options before asking for input

--- Meni/menu.py
+++ Meni/menu.py
@@ -5,10 +5,10 @@ class Menu:
     def ask_for_menu_option(self):
         option = "-1"
         while option not in self.options:
+            self.print_menu_options()
             option = input("Izaberite opciju: ")
         return option
 
     def print_menu_options(self):
-        print()
         for key, value in self.options.items():
             print(f"{key} -> {value}")
