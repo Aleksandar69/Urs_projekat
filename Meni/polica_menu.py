f74bb1d680e85ca56198a9aa427162dd74c6db36
Goran Ninkovic <warhawk@hotmail.rs>
2019-06-19 18:45:54 +0200

    add Menu class

---
+++ Meni/polica_menu.py
@@ -0,0 +1,8 @@
+from Meni.menu import Menu
+
+
+class PolicaMenu(Menu):
+    def __init__(self):
+        self.options = {"0": "Nazad", "1": "Prikaz polica", "2": "Dodavanje polica",
+                        "3": "Izmjena polica", "4": "Pretraga polica", "5": "Sortiranje polica", "6": "Prikaz artikala na polici"}
+        super().__init__(self.options)
