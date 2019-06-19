c4f725643bbd769707c335e63e54995cbd17800e
Goran Ninkovic <warhawk@hotmail.rs>
2019-06-19 19:42:26 +0200

    add welcome message

--- Meni/polica_menu.py
+++ Meni/polica_menu.py
@@ -6,3 +6,7 @@ class PolicaMenu(Menu):
         self.options = {"0": "Nazad", "1": "Prikaz polica", "2": "Dodavanje polica",
                         "3": "Izmjena polica", "4": "Pretraga polica", "5": "Sortiranje polica", "6": "Prikaz artikala na polici"}
         super().__init__(self.options)
+
+    def print_welcome_message(self):
+        print()
+        print("Rad sa policama")
